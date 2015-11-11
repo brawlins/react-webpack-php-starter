e5d5b2ed03f29271dcc5ad4146be1a7aeab5c6a3
Brett Rawlins <brettswww@gmail.com>
2015-11-11 13:25:39 -0700

    Use browser's History API rather than the default has history.
    This makes URLs cleaner ('/about' instead of '/#/about') but is not
    supported in older browsers. You also must configure your server to
    handle the real URL. It should route all requests to the index file and
    let react-router handle loading the appropriate component.

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ console.log('main.js');
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, Redirect } from 'react-router';
+// use the browser's History API rather than the default hash history
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 import App from './components/App.js';
 import HomePage from './components/home/HomePage.js';
 import AboutPage from './components/about/AboutPage.js';
 
 render((
-	<Router>
+	<Router history={createBrowserHistory()}>
 		<Route path="/" component={App}>
 			<IndexRoute component={HomePage} />
 			<Route path="about" component={AboutPage}/>
