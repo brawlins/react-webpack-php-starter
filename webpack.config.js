fb864b3e504a325fac7dde27625054e47f1f3cd6
Brett Rawlins <brettswww@gmail.com>
2015-11-10 20:53:03 -0700

    Fix loader to include woff2 fonts.

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,10 @@ module.exports = {
 			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
 			{ test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
 			{ test: /\.css$/, loader: 'style!css' },
-			{ test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
-			{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
-			{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
-			{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" }			
+			{ test: /\.woff(2?)(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+			{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+			{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+			{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }			
 		]
 	}, 
 	resolve: {
